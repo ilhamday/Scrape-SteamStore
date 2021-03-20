c3834701df22fa2ca770b8135acdeb08c530f447
ilhamday <ilhamyudha00@gmail.com>
2021-03-21 04:34:39 +0700

    Add flask to show data
    All find feature from bs4, done in grid_template.html

--- main.py
+++ main.py
@@ -1,44 +1,57 @@
 import requests
 import requests_cache
 from bs4 import BeautifulSoup
+from flask import Flask, render_template
 
-requests_cache.install_cache('steamstore_chace')
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:86.0) Gecko/20100101 Firefox/86.0'
-}
-
-url = 'https://store.steampowered.com/search/?filter=topsellers'
-
-res = requests.get(url, headers=headers)
-soup = BeautifulSoup(res.text, 'html.parser')
-
+# requests_cache.install_cache('steamstore_chace')
 # Check if get the data from cache
-print(f"Used Cache: {res.from_cache}")
+# print(f"Used Cache: {res.from_cache}")
 
-steams = soup.findAll('a', attrs={'class':'search_result_row ds_collapse_flag'})
 
-titles = []
-for s in steams:
-    print('---')
-    title = s.find('span', attrs={'class': 'title'}).text
+# initialization flask
+app = Flask(__name__)
 
-    # Get span element
-    platform_list = []
-    platform = s.find('p').findAll('span')
+@app.route('/')
+def top_sellers():
 
-    # Loop through span
-    for pl in platform:
-        # if attribute class has more than 1 value, get the last one
-        if len(pl.attrs['class']) > 1:
-            p = pl.attrs['class'][1]
-        else:
-            p = pl.attrs['class'][0]
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:86.0) Gecko/20100101 Firefox/86.0'
+    }
+
+    url = 'https://store.steampowered.com/search/?filter=topsellers'
+
+    res = requests.get(url, headers=headers)
+    soup = BeautifulSoup(res.text, 'html.parser')
+
+    steams = soup.findAll('a', attrs={'class':'search_result_row ds_collapse_flag'})
+
+    return render_template('grid_template.html', top_sellers=steams)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
+# titles = []
+# for s in steams:
+#     print('---')
+#     title = s.find('span', attrs={'class': 'title'}).text
+
+#     # Get span element
+#     platform_list = []
+#     platform = s.find('p').findAll('span')
+
+#     # Loop through span
+#     for pl in platform:
+#         # if attribute class has more than 1 value, get the last one
+#         if len(pl.attrs['class']) > 1:
+#             p = pl.attrs['class'][1]
+#         else:
+#             p = pl.attrs['class'][0]
         
-        platform_list.append(p)
+#         platform_list.append(p)
 
-    price = s.find('div', attrs={'class': 'search_price'}).text
-    img_link = s.find('img').attrs
-    img_link = img_link['src']
+#     price = s.find('div', attrs={'class': 'search_price'}).text
+#     img_link = s.find('img').attrs
+#     img_link = img_link['src']
    
-    print(f'{title} -- {price} -- {img_link} -- {platform_list}')
+#     print(f'{title} -- {price} -- {img_link} -- {platform_list}')
